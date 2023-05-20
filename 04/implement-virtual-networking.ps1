9ffcb622cf3a6c53f6fd4870911cd648437e4a78
cgoode1202 <cgoode1202@gmail.com>
2023-05-20 11:18:19 -0700

    added commands for creating a network security group with a rule to allow RDP, and associating that NSG with both NICs

--- 04/implement-virtual-networking.ps1
+++ 04/implement-virtual-networking.ps1
@@ -19,17 +19,45 @@ New-AzVirtualNetwork -Name $vnetName -ResourceGroupName $rgName -Location $locat
 New-AzResourceGroupDeployment -ResourceGroupName $rgName -TemplateFile $HOME/az104-04-vms-loop-template.json -TemplateParameterFile $HOME/az104-04-vms-loop-parameters.json
 
 # Create public IP addresses which will be used to associate with NICs of both VMs
-$pip0 = New-AzPublicIpAddress -Name 'az104-04-pip0' -ResourceGroupName $rgName -AllocationMethod Static -Location $location -Sku 'Standard' -IpAddressVersion 'IPv4'
-$pip1 = New-AzPublicIpAddress -Name 'az104-04-pip1' -ResourceGroupName $rgName -AllocationMethod Static -Location $location -Sku 'Standard' -IpAddressVersion 'IPv4'
+$pip0 = New-AzPublicIpAddress -Name 'az104-04-pip0' -ResourceGroupName $rgName -AllocationMethod 'Static' -Location $location -Sku 'Standard' -IpAddressVersion 'IPv4'
+$pip1 = New-AzPublicIpAddress -Name 'az104-04-pip1' -ResourceGroupName $rgName -AllocationMethod 'Static' -Location $location -Sku 'Standard' -IpAddressVersion 'IPv4'
 
 # Retrieve data about both network interface cards
 $nic0 = Get-AzNetworkInterface -Name 'az104-04-nic0' -ResourceGroupName $rgName 
 $nic1 = Get-AzNetworkInterface -Name 'az104-04-nic1' -ResourceGroupName $rgName
 
 # Associate both public IP addresses with IP configurations for both NICs
-$nic0 | Set-AzNetworkInterfaceIpConfig -Name ipconfig1 -PublicIpAddress $pip0 -Subnet $subnet0
-$nic1 | Set-AzNetworkInterfaceIpConfig -Name ipconfig1 -PublicIpAddress $pip1 -Subnet $subnet1
+$nic0 | Set-AzNetworkInterfaceIpConfig -Name 'ipconfig1' -PublicIpAddress $pip0 -Subnet $subnet0
+$nic1 | Set-AzNetworkInterfaceIpConfig -Name 'ipconfig1' -PublicIpAddress $pip1 -Subnet $subnet1
 
 # Write the new IP configurations to the network interfaces
 $nic0 | Set-AzNetworkInterface
 $nic1 | Set-AzNetworkInterface
+
+# Still need to set assignment of ipconfig1 from Dynamic to Static in the Portal, shell commands to be added later...
+
+# Lab instructions require you to download the RDP file for vm0, attempt to connect, and note that the attmpted failed
+
+# Stop both lab VMs before we create network security groups
+Stop-AzVM -ResourceGroupName $rgName -Name 'az104-04-vm0'
+Stop-AzVM -ResourceGroupName $rgName -Name 'az104-04-vm1'
+
+# Create a rule to allow RDP inbound and a new network security group using that rule
+$rule1 = New-AzNetworkSecurityRuleConfig -Name AllowRDPInbound -Description 'Allow RDP' -Access Allow -Protocol TCP -Direction Inbound -Priority 300 -SourceAddressPrefix Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389
+$nsg = New-AzNetworkSecurityGroup -ResourceGroupName $rgName -Location $location -Name 'az104-04-nsg01' -SecurityRules $rule1
+
+# Associate the NSG with nic0
+$nic0 = Get-AzNetworkInterface -Name 'az104-04-nic0' -ResourceGroupName $rgName
+$nic0.NetworkSecurityGroup = $nsg
+$nic0 | Set-AzNetworkInterface
+
+# Associate the NSG with nic1
+$nic1 = Get-AzNetworkInterface -Name 'az104-04-nic1' -ResourceGroupName $rgName
+$nic1.NetworkSecurityGroup = $nsg
+$nic1 | Set-AzNetworkInterface
+
+# Start up both VMs
+Start-AzVM -ResourceGroupName $rgName -Name 'az104-04-vm0'
+Start-AzVM -ResourceGroupName $rgName -Name 'az104-04-vm1'
+
+# At this point the lab instructs you to download the RDP file for vm0 and connect, it should work now that we've configured the NSG to allow RDP
