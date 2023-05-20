cda5bfd4b2347938c43395d77a2fb7b199b4fece
cgoode1202 <cgoode1202@gmail.com>
2023-05-20 09:37:23 -0700

    added commands for creating public IP addresses and associating them to network interfaces

--- 04/implement-virtual-networking.ps1
+++ 04/implement-virtual-networking.ps1
@@ -14,3 +14,22 @@ $subnet1 = New-AzVirtualNetworkSubnetConfig -Name 'subnet1' -AddressPrefix '10.4
 
 # Create a new virtual network using the CIDR 10.40.0.0/20 as the address prefix and the two subnets
 New-AzVirtualNetwork -Name $vnetName -ResourceGroupName $rgName -Location $location -AddressPrefix '10.40.0.0/20' -Subnet $subnet0,$subnet1
+
+# Deploy virtual machines using the template files included in the Labs
+New-AzResourceGroupDeployment -ResourceGroupName $rgName -TemplateFile $HOME/az104-04-vms-loop-template.json -TemplateParameterFile $HOME/az104-04-vms-loop-parameters.json
+
+# Create public IP addresses which will be used to associate with NICs of both VMs
+$pip0 = New-AzPublicIpAddress -Name 'az104-04-pip0' -ResourceGroupName $rgName -AllocationMethod Static -Location $location -Sku 'Standard' -IpAddressVersion 'IPv4'
+$pip1 = New-AzPublicIpAddress -Name 'az104-04-pip1' -ResourceGroupName $rgName -AllocationMethod Static -Location $location -Sku 'Standard' -IpAddressVersion 'IPv4'
+
+# Retrieve data about both network interface cards
+$nic0 = Get-AzNetworkInterface -Name 'az104-04-nic0' -ResourceGroupName $rgName 
+$nic1 = Get-AzNetworkInterface -Name 'az104-04-nic1' -ResourceGroupName $rgName
+
+# Associate both public IP addresses with IP configurations for both NICs
+$nic0 | Set-AzNetworkInterfaceIpConfig -Name ipconfig1 -PublicIpAddress $pip0 -Subnet $subnet0
+$nic1 | Set-AzNetworkInterfaceIpConfig -Name ipconfig1 -PublicIpAddress $pip1 -Subnet $subnet1
+
+# Write the new IP configurations to the network interfaces
+$nic0 | Set-AzNetworkInterface
+$nic1 | Set-AzNetworkInterface
